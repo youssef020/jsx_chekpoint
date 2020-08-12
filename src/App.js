b328265c6c81f580df0eb9449f5e869ceaf03c0f
youssef <youssefbennajeh111@gmail.com>
2020-08-12 12:34:45 +0100

    jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Image from './geraltciri.jpg'
+import './style.css'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{border:'solid 1px black', maxWidth:"100vw"}}>
+    <h1 className="title red">Geralt of Rivia </h1>
+   <br />
+   <img src={Image} />
+   <img src="/geralt.png" />
+
+   </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
   );
+
 }
 
 export default App;
